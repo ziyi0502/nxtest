71f9558c3e042f755d592a98e179b5e19f77600e
ziyiding <ziyiding@microsoft.com>
2022-07-21 17:09:20 +0800

    Auth init

--- my-workspace/apps/blog/src/app.controller.ts
+++ my-workspace/apps/blog/src/app.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards ,Request} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 
 @Controller()
 export class AppController {
-
+  @UseGuards(LocalAuthGuard)
   @Get()
-  getData() {
-    return "hello ziyi";
+  getData(@Request() req) {
+    return req.user;
   }
 }
--- my-workspace/apps/blog/src/app.module.ts
+++ my-workspace/apps/blog/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule,AuthModule],
   controllers: [AppController],
   providers: [],
 })
