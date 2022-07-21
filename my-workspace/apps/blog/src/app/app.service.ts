08fa799e016b4057867e6a694a34342d6553ed1d
ziyiding <ziyiding@microsoft.com>
2022-07-21 16:47:49 +0800

    Init modules structor

--- my-workspace/apps/blog/src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getData(): { message: string } {
-    console.log('dd');
-    return { message: 'Welcome to b'};
-  }
-}
