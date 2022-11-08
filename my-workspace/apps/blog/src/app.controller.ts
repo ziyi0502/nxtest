90f65e82f9056fffa425c82e10a374a0f61b151d
ziyiding <ziyiding@microsoft.com>
2022-11-08 13:10:43 +0800

    test

--- my-workspace/apps/blog/src/app.controller.ts
+++ my-workspace/apps/blog/src/app.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get, UseGuards ,Request} from '@nestjs/common';
+import { Controller, Get, UseGuards ,Request, Post} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guards';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Get()
   getData(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
